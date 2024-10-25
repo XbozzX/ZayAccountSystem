85f25434128c4a0913116c9af248f1b39f69017d
XbozzX <afham821@gmail.com>
2024-10-25 19:04:56 +0800

    updated: main.jsx & deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,10 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
