6617cba86f2588b6cf069c248465a53bf83c0b28
XbozzX <afham821@gmail.com>
2024-10-25 17:06:57 +0800

    updated: basename for page 404 issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="ZayAccountSystem">
-      <App />
+    <BrowserRouter basename="/ZayAccountSystem/">
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
